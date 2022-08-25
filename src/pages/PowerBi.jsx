0c24c3d7065a33075c14ebc8f7baa4b94289e594
gabrielhofmann <hofmannmarinho@gmail.com>
2022-08-25 16:10:28 -0300

    update

--- src/pages/PowerBi.jsx
+++ src/pages/PowerBi.jsx
@@ -8,6 +8,8 @@ import "./PowerBi.scss";
 import Filter from "../components/Filter";
 import Pagination from "../components/Pagination";
 
+import logo from "../assets/logo.svg";
+
 const services = new Services();
 
 export default class PowerBi extends Component {
@@ -150,6 +152,14 @@ export default class PowerBi extends Component {
           </div>
 
           <div>
+            <h1>{this.state.powerBiData.mau}</h1>
+            <p>
+              <span className="material-symbols-rounded">shopping_bag</span>
+              MAU
+            </p>
+          </div>
+
+          <div>
             <h1>{this.state.numberOfRestaurants}</h1>
             <p>
               <span className="material-symbols-rounded">store</span>
@@ -202,11 +212,6 @@ export default class PowerBi extends Component {
 
         <section className="pageContainer powerBiContainer">
           <div className="smallCardsContainer">
-            <div className="smallCard">
-              <h1>MAU</h1>
-              <p>{this.state.powerBiData.mau}</p>
-            </div>
-
             <div className="smallCard smallListCard">
               <h1>Novos Usuários</h1>
               <div className="listRow">
@@ -227,6 +232,31 @@ export default class PowerBi extends Component {
                   <li key="monthUsers">
                     <p>
                       Mês: <span>{this.state.powerBiData.newUsers.month}</span>
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="smallCard smallListCard">
+              <h1>Pedidos</h1>
+              <div className="listRow">
+                <ul>
+                  <li key="todayRevenue">
+                    <p>
+                      Hoje: <span>{this.state.powerBiData.revenue.today}</span>
+                    </p>
+                  </li>
+
+                  <li key="weekRevenue">
+                    <p>
+                      Semana: <span>{this.state.powerBiData.revenue.week}</span>
+                    </p>
+                  </li>
+
+                  <li key="monthRevenue">
+                    <p>
+                      Mês: <span>{this.state.powerBiData.revenue.month}</span>
                     </p>
                   </li>
                 </ul>
@@ -326,31 +356,36 @@ export default class PowerBi extends Component {
           <Pagination target="orders" callback={this.setOrders} />
 
           <div className="orderCardsContainer">
-            <div className="orderCardsContainerNameplates">
-              <strong>ID</strong>
-              <strong>Usuário</strong>
-              <strong>Restaurante</strong>
-              <strong>Criado em</strong>
-              <strong>Atualizado em</strong>
-              <strong>Status</strong>
-              <strong>Valor total</strong>
-            </div>
+            <strong style={{ marginLeft: "2.7rem" }}>ID</strong>
+            <strong>Cliente</strong>
+            <strong>Restaurante</strong>
+            <strong>Criado em</strong>
+            <strong>Atualizado em</strong>
+            <strong>Status</strong>
+            <strong>Total</strong>
 
-            {this.state.orders.map((element) => {
+            {this.state.orders.map((order) => {
               return (
                 <div className="orderCard">
-                  <div className="orderCardInfo">
-                    <p>{element.id}</p>
-                    <p>{element.user.username}</p>
-                    <p>{element.restaurant.name}</p>
-                    <p>{new Date(element.createdAt).toLocaleDateString()}</p>
-                    <p>{new Date(element.updatedAt).toLocaleDateString()}</p>
-                    <p>{element.status}</p>
-                    <p>R${element.history.payment.total}</p>
-                  </div>
-                  <div className="orderCardButtons">
+                  <strong>#ID {order.id}</strong>
+                  <strong>{order.user.username}</strong>
+                  <strong>{order.restaurant.name}</strong>
+                  <strong>
+                    {new Date(order.createdAt).toLocaleDateString()}
+                  </strong>
+                  <strong>
+                    {new Date(order.updatedAt).toLocaleDateString()}
+                  </strong>
+                  <strong>{order.status}</strong>
+                  <strong style={{ color: "#52e899" }}>
+                    R$ {order.history.payment.total}
+                  </strong>
+
+                  <div className="cardButtons">
                     <button>Ver usuário</button>
+
                     <button>Ver restaurante</button>
+
                     <button>Mais</button>
                   </div>
                 </div>
@@ -368,28 +403,56 @@ export default class PowerBi extends Component {
           <Pagination target="restaurants" callback={this.setRestaurants} />
 
           <div className="restaurantCardsContainer">
-            <div className="restaurantCardsContainerNameplates">
-              <strong>Nome/ID</strong>
-              <strong>Status</strong>
-              <strong>Categoria</strong>
-              <strong>Email</strong>
-              <strong>Telefone</strong>
-              <strong>Rating</strong>
-            </div>
-            {this.state.restaurants.map((element) => {
+            <strong></strong>
+            <strong>Nome/ID</strong>
+            <strong>Status</strong>
+            <strong>Categoria</strong>
+            <strong>Aberto</strong>
+            <strong>Telefone</strong>
+            <strong>Rating</strong>
+
+            {this.state.restaurants.map((restaurant) => {
+              console.log(restaurant);
+
               return (
                 <div className="restaurantCard">
-                  <div className="restaurantImage"></div>
-                  <p>
-                    {element.name}
-                    <br />
-                    {`#ID${element.id}`}
-                  </p>
-                  <p>{element.status}</p>
-                  <p>{element.category}</p>
-                  <p>{element.email}</p>
-                  <p>{element.phoneNumber}</p>
-                  <p>{element.rating}</p>
+                  <div
+                    className="restaurantCardImage"
+                    style={{
+                      backgroundImage: restaurant.thumbnailImageUrl
+                        ? `url(${restaurant.thumbnailImageUrl})`
+                        : `url(${logo})`,
+                    }}
+                  ></div>
+
+                  <strong style={{ color: "#717171", fontSize: "1.6rem" }}>
+                    {restaurant.name} <br />
+                    <span
+                      style={{
+                        fontSize: "1.3rem",
+                        display: "flex",
+                        marginTop: "-.3rem",
+                      }}
+                    >
+                      #ID{restaurant.id}
+                    </span>
+                  </strong>
+
+                  <strong>{restaurant.status}</strong>
+
+                  <strong>{restaurant.category}</strong>
+
+                  <strong
+                    style={{ color: restaurant.isOpen ? "green" : "red" }}
+                  >
+                    {restaurant.isOpen ? "SIM" : "NÃO"}
+                  </strong>
+
+                  <strong>{restaurant.phoneNumber}</strong>
+
+                  <strong style={{ color: "#52e899" }}>
+                    {restaurant.rating}
+                  </strong>
                 </div>
               );
             })}
