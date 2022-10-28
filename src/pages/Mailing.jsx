7c27695d268036eb1929b4cac49ef7b4cd71cdc9
gabrielhofmann <hofmannmarinho@gmail.com>
2022-10-28 10:35:49 -0300

    update

--- src/pages/Mailing.jsx
+++ src/pages/Mailing.jsx
@@ -9,55 +9,6 @@ import $ from "jquery";
 import "./Mailing.scss";
 import axios from "axios";
 
-import { Editor } from "@tinymce/tinymce-react";
-
-// TinyMCE so the global var exists
-// eslint-disable-next-line no-unused-vars
-import tinymce from "tinymce/tinymce";
-// DOM model
-import "../../public/tinymce/models/dom/model.min.js";
-// Theme
-import "../../public/tinymce/themes/silver/theme.min.js";
-// Toolbar icons
-import "../../public/tinymce/icons/default/icons.min.js";
-// Editor styles
-import "../../public/tinymce/skins/ui/oxide/skin.min.css";
-
-// importing the plugin js.
-// if you use a plugin that is not listed here the editor will fail to load
-import "../../public/tinymce/plugins/advlist/plugin.min.js";
-import "../../public/tinymce/plugins/anchor/plugin.min.js";
-import "../../public/tinymce/plugins/autolink/plugin.min.js";
-import "../../public/tinymce/plugins/autoresize/plugin.min.js";
-import "../../public/tinymce/plugins/autosave/plugin.min.js";
-import "../../public/tinymce/plugins/charmap/plugin.min.js";
-import "../../public/tinymce/plugins/code/plugin.min.js";
-import "../../public/tinymce/plugins/codesample/plugin.min.js";
-import "../../public/tinymce/plugins/directionality/plugin.min.js";
-import "../../public/tinymce/plugins/emoticons/plugin.min.js";
-import "../../public/tinymce/plugins/fullscreen/plugin.min.js";
-import "../../public/tinymce/plugins/help/plugin.min.js";
-import "../../public/tinymce/plugins/image/plugin.min.js";
-import "../../public/tinymce/plugins/importcss/plugin.min.js";
-import "../../public/tinymce/plugins/insertdatetime/plugin.min.js";
-import "../../public/tinymce/plugins/link/plugin.min.js";
-import "../../public/tinymce/plugins/lists/plugin.min.js";
-import "../../public/tinymce/plugins/media/plugin.min.js";
-import "../../public/tinymce/plugins/nonbreaking/plugin.min.js";
-import "../../public/tinymce/plugins/pagebreak/plugin.min.js";
-import "../../public/tinymce/plugins/preview/plugin.min.js";
-import "../../public/tinymce/plugins/quickbars/plugin.min.js";
-import "../../public/tinymce/plugins/save/plugin.min.js";
-import "../../public/tinymce/plugins/searchreplace/plugin.min.js";
-import "../../public/tinymce/plugins/table/plugin.min.js";
-import "../../public/tinymce/plugins/template/plugin.min.js";
-import "../../public/tinymce/plugins/visualblocks/plugin.min.js";
-import "../../public/tinymce/plugins/visualchars/plugin.min.js";
-import "../../public/tinymce/plugins/wordcount/plugin.min.js";
-
-// importing plugin resources
-import "tinymce/plugins/emoticons/js/emojis";
-
 const services = new Services();
 
 export default class Mailing extends Component {
@@ -132,6 +83,247 @@ export default class Mailing extends Component {
         if (active == "true") {
           services.handleMenuToggle();
         }
+      });
+
+      CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
+        // https://ckeditor.com/docs/ckeditor5/latest/features/toolbar/toolbar.html#extended-toolbar-configuration-format
+        toolbar: {
+          items: [
+            "exportPDF",
+            "exportWord",
+            "|",
+            "findAndReplace",
+            "selectAll",
+            "|",
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "strikethrough",
+            "underline",
+            "code",
+            "subscript",
+            "superscript",
+            "removeFormat",
+            "|",
+            "bulletedList",
+            "numberedList",
+            "todoList",
+            "|",
+            "outdent",
+            "indent",
+            "|",
+            "undo",
+            "redo",
+            "-",
+            "fontSize",
+            "fontFamily",
+            "fontColor",
+            "fontBackgroundColor",
+            "highlight",
+            "|",
+            "alignment",
+            "|",
+            "link",
+            "insertImage",
+            "blockQuote",
+            "insertTable",
+            "mediaEmbed",
+            "codeBlock",
+            "htmlEmbed",
+            "|",
+            "specialCharacters",
+            "horizontalLine",
+            "pageBreak",
+            "|",
+            "textPartLanguage",
+            "|",
+            "sourceEditing",
+          ],
+          shouldNotGroupWhenFull: true,
+        },
+        // Changing the language of the interface requires loading the language file using the <script> tag.
+        // language: 'es',
+        list: {
+          properties: {
+            styles: true,
+            startIndex: true,
+            reversed: true,
+          },
+        },
+        // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html#configuration
+        heading: {
+          options: [
+            {
+              model: "paragraph",
+              title: "Paragraph",
+              class: "ck-heading_paragraph",
+            },
+            {
+              model: "heading1",
+              view: "h1",
+              title: "Heading 1",
+              class: "ck-heading_heading1",
+            },
+            {
+              model: "heading2",
+              view: "h2",
+              title: "Heading 2",
+              class: "ck-heading_heading2",
+            },
+            {
+              model: "heading3",
+              view: "h3",
+              title: "Heading 3",
+              class: "ck-heading_heading3",
+            },
+            {
+              model: "heading4",
+              view: "h4",
+              title: "Heading 4",
+              class: "ck-heading_heading4",
+            },
+            {
+              model: "heading5",
+              view: "h5",
+              title: "Heading 5",
+              class: "ck-heading_heading5",
+            },
+            {
+              model: "heading6",
+              view: "h6",
+              title: "Heading 6",
+              class: "ck-heading_heading6",
+            },
+          ],
+        },
+        // https://ckeditor.com/docs/ckeditor5/latest/features/editor-placeholder.html#using-the-editor-configuration
+        placeholder: "Welcome to CKEditor 5!",
+        // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-family-feature
+        fontFamily: {
+          options: [
+            "default",
+            "Arial, Helvetica, sans-serif",
+            "Courier New, Courier, monospace",
+            "Georgia, serif",
+            "Lucida Sans Unicode, Lucida Grande, sans-serif",
+            "Tahoma, Geneva, sans-serif",
+            "Times New Roman, Times, serif",
+            "Trebuchet MS, Helvetica, sans-serif",
+            "Verdana, Geneva, sans-serif",
+          ],
+          supportAllValues: true,
+        },
+        // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-size-feature
+        fontSize: {
+          options: [10, 12, 14, "default", 18, 20, 22],
+          supportAllValues: true,
+        },
+        // Be careful with the setting below. It instructs CKEditor to accept ALL HTML markup.
+        // https://ckeditor.com/docs/ckeditor5/latest/features/general-html-support.html#enabling-all-html-features
+        htmlSupport: {
+          allow: [
+            {
+              name: /.*/,
+              attributes: true,
+              classes: true,
+              styles: true,
+            },
+          ],
+        },
+        // Be careful with enabling previews
+        // https://ckeditor.com/docs/ckeditor5/latest/features/html-embed.html#content-previews
+        htmlEmbed: {
+          showPreviews: true,
+        },
+        // https://ckeditor.com/docs/ckeditor5/latest/features/link.html#custom-link-attributes-decorators
+        link: {
+          decorators: {
+            addTargetToExternalLinks: true,
+            defaultProtocol: "https://",
+            toggleDownloadable: {
+              mode: "manual",
+              label: "Downloadable",
+              attributes: {
+                download: "file",
+              },
+            },
+          },
+        },
+        // https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html#configuration
+        mention: {
+          feeds: [
+            {
+              marker: "@",
+              feed: [
+                "@apple",
+                "@bears",
+                "@brownie",
+                "@cake",
+                "@cake",
+                "@candy",
+                "@canes",
+                "@chocolate",
+                "@cookie",
+                "@cotton",
+                "@cream",
+                "@cupcake",
+                "@danish",
+                "@donut",
+                "@dragée",
+                "@fruitcake",
+                "@gingerbread",
+                "@gummi",
+                "@ice",
+                "@jelly-o",
+                "@liquorice",
+                "@macaroon",
+                "@marzipan",
+                "@oat",
+                "@pie",
+                "@plum",
+                "@pudding",
+                "@sesame",
+                "@snaps",
+                "@soufflé",
+                "@sugar",
+                "@sweet",
+                "@topping",
+                "@wafer",
+              ],
+              minimumCharacters: 1,
+            },
+          ],
+        },
+        // The "super-build" contains more premium features that require additional configuration, disable them below.
+        // Do not turn them on unless you read the documentation and know how to configure them and setup the editor.
+        removePlugins: [
+          // These two are commercial, but you can try them out without registering to a trial.
+          // 'ExportPdf',
+          // 'ExportWord',
+          "CKBox",
+          "CKFinder",
+          "EasyImage",
+          // This sample uses the Base64UploadAdapter to handle image uploads as it requires no configuration.
+          // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/base64-upload-adapter.html
+          // Storing images as Base64 is usually a very bad idea.
+          // Replace it on production website with other solutions:
+          // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/image-upload.html
+          // 'Base64UploadAdapter',
+          "RealTimeCollaborativeComments",
+          "RealTimeCollaborativeTrackChanges",
+          "RealTimeCollaborativeRevisionHistory",
+          "PresenceList",
+          "Comments",
+          "TrackChanges",
+          "TrackChangesData",
+          "RevisionHistory",
+          "Pagination",
+          "WProofreader",
+          // Careful, with the Mathtype plugin CKEditor will not load when loading this sample
+          // from a local file system (file://) - load this site via HTTP server if you enable MathType
+          "MathType",
+        ],
       });
 
       this.setState({ usersOptions: usersOptions });
@@ -292,43 +484,7 @@ export default class Mailing extends Component {
               </Form.Group>
             </div>
 
-            <Editor
-              id="#tiny"
-              init={{
-                height: 500,
-                menubar: false,
-                plugins: [
-                  "advlist",
-                  "autolink",
-                  "lists",
-                  "link",
-                  "image",
-                  "charmap",
-                  "print",
-                  "preview",
-                  "anchor",
-                  "searchreplace",
-                  "visualblocks",
-                  "code",
-                  "fullscreen",
-                  "insertdatetime",
-                  "media",
-                  "table",
-                  "paste",
-                  "code",
-                  "help",
-                  "wordcount",
-                ],
-                toolbar:
-                  "undo redo | " +
-                  "bold italic charmap | alignleft aligncenter " +
-                  "alignright alignjustify | outdent indent | " +
-                  "| link image media charmap |" +
-                  "| removeformat | help preview ",
-                content_style:
-                  "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-              }}
-            />
+            <div id="editor"></div>
 
             <button id="mailingFormButton" type="submit">
               Enviar
